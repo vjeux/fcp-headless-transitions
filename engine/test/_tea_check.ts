bb7ae40461400c29a4136298d5a131a2b96b5501
Christopher Chedeau <vjeux@meta.com>
2026-07-24 18:40:47 +0100

    cleanup: remove accidentally-committed temp _tea_check.ts probe

--- engine/test/_tea_check.ts
+++
@@ -1,4 +0,0 @@
-import { teaRand } from '../src/compositor/emitter-sim.js';
-for (const [a,b,c] of [[0,0,0],[1,0,0],[0,1,0],[18155,0,0],[25785,1,0]] as [number,number,number][]) {
-  console.log(`teaRand(${a},${b},${c}) = ${teaRand(a,b,c).toFixed(6)}`);
-}
