50e83a3e0337ec0162d6f750adac7297813ff746
Christopher Chedeau <vjeux@meta.com>
2026-07-28 05:31:22 +0100

    raw-port: transcribe FFSoundDesignerIISupportDelegate — gate PASS

---
+++ raw-port/re/disasm/Flexo.FFSoundDesignerIISupportDelegate.resourceLoaderDelegate.s
@@ -0,0 +1,7 @@
+__ZNK32FFSoundDesignerIISupportDelegate22resourceLoaderDelegateEv:
+0000000000e326c0	pushq	%rbp
+0000000000e326c1	movq	%rsp, %rbp
+0000000000e326c4	movq	(%rdi), %rax
+0000000000e326c7	popq	%rbp
+0000000000e326c8	retq
+0000000000e326c9	nopl	(%rax)
