e23a46abf00f9990863618bf41c33f562ff56fa2
Christopher Chedeau <vjeux@meta.com>
2026-07-28 06:30:04 +0100

    raw-port: transcribe POTPaddingPolicy — gate PASS

---
+++ raw-port/re/disasm/Helium.POTPaddingPolicy.D2Ev.s
@@ -0,0 +1,6 @@
+__ZN16POTPaddingPolicyD2Ev:
+0000000000045080	pushq	%rbp
+0000000000045081	movq	%rsp, %rbp
+0000000000045084	popq	%rbp
+0000000000045085	jmp	__ZN8HGObjectD2Ev               ## HGObject::~HGObject()
+000000000004508a	nopw	(%rax,%rax)
