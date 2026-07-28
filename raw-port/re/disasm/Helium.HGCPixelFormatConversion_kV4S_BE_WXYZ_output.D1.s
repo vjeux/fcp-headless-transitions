ebcb664f1898c529713fa0f6a093f0ca353bf469
Christopher Chedeau <vjeux@meta.com>
2026-07-28 04:20:12 +0100

    raw-port: transcribe HGCPixelFormatConversion_kV4S_BE_WXYZ_output — gate PASS

---
+++ raw-port/re/disasm/Helium.HGCPixelFormatConversion_kV4S_BE_WXYZ_output.D1.s
@@ -0,0 +1,6 @@
+__ZN44HGCPixelFormatConversion_kV4S_BE_WXYZ_outputD1Ev:
+00000000000fd3a0	pushq	%rbp
+00000000000fd3a1	movq	%rsp, %rbp
+00000000000fd3a4	popq	%rbp
+00000000000fd3a5	jmp	__ZN44HgcPixelFormatConversion_kV4S_BE_WXYZ_outputD2Ev ## HgcPixelFormatConversion_kV4S_BE_WXYZ_output::~HgcPixelFormatConversion_kV4S_BE_WXYZ_output()
+00000000000fd3aa	nopw	(%rax,%rax)
