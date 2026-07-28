8544931bcb928173a741e2bd4f07a0db445f3f36
Christopher Chedeau <vjeux@meta.com>
2026-07-28 07:18:59 +0100

    raw-port: transcribe OZOverrideFCPColorSpaceUndo — gate PASS

---
+++ raw-port/re/disasm/OZOverrideFCPColorSpaceUndo.~OZOverrideFCPColorSpaceUndo.s
@@ -0,0 +1,6 @@
+__ZN27OZOverrideFCPColorSpaceUndoD0Ev:
+0000000000101740	pushq	%rbp
+0000000000101741	movq	%rsp, %rbp
+0000000000101744	popq	%rbp
+0000000000101745	jmp	0x6dfc36                        ## symbol stub for: __ZdlPv
+000000000010174a	nopw	(%rax,%rax)
