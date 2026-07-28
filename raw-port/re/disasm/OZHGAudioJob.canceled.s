bb45e6932f8822017f45a519e96fcbd0a717b8ae
Christopher Chedeau <vjeux@meta.com>
2026-07-28 07:03:07 +0100

    raw-port: transcribe OZHGAudioJob @0x6365e0/0x636760/0x6369a0/0x6369c0 — gate PASS

---
+++ raw-port/re/disasm/OZHGAudioJob.canceled.s
@@ -0,0 +1,9 @@
+__ZN12OZHGAudioJob8canceledEv:
+00000000006369a0	pushq	%rbp
+00000000006369a1	movq	%rsp, %rbp
+00000000006369a4	movq	%rdi, %rdx
+00000000006369a7	movq	0x80(%rdi), %rdi
+00000000006369ae	movq	0x2e0aa3(%rip), %rsi
+00000000006369b5	popq	%rbp
+00000000006369b6	jmpq	*0x1ef66c(%rip)                 ## Objc message: -[%rdi updateMasterTracksArray]
+00000000006369bc	nopl	(%rax)
