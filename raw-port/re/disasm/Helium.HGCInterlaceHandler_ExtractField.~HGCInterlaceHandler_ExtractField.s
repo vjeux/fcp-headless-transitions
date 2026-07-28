26118fde7eb21587d8bb020ac2bb1f7998274b4e
Christopher Chedeau <vjeux@meta.com>
2026-07-28 02:14:19 +0100

    raw-port: transcribe HGCInterlaceHandler_ExtractField @0x093340/0x093360/0x0933c0 — gate PASS

---
+++ raw-port/re/disasm/Helium.HGCInterlaceHandler_ExtractField.~HGCInterlaceHandler_ExtractField.s
@@ -0,0 +1,13 @@
+__ZN32HGCInterlaceHandler_ExtractFieldD0Ev:
+0000000000093340	pushq	%rbp
+0000000000093341	movq	%rsp, %rbp
+0000000000093344	pushq	%rbx
+0000000000093345	pushq	%rax
+0000000000093346	movq	%rdi, %rbx
+0000000000093349	callq	__ZN32HgcInterlaceHandler_ExtractFieldD2Ev ## HgcInterlaceHandler_ExtractField::~HgcInterlaceHandler_ExtractField()
+000000000009334e	movq	%rbx, %rdi
+0000000000093351	addq	$0x8, %rsp
+0000000000093355	popq	%rbx
+0000000000093356	popq	%rbp
+0000000000093357	jmp	__ZN8HGObjectdlEPv              ## HGObject::operator delete(void*)
+000000000009335c	nopl	(%rax)
