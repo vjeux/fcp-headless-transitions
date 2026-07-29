d03693de6e8c9eb929617768c93100441aeb75b0
Christopher Chedeau <vjeux@meta.com>
2026-07-29 01:40:13 +0100

    wip: HGApply3DLUT disasm captured

---
+++ raw-port/re/disasm/Helium.HGApply3DLUT.Init.s
@@ -0,0 +1,10 @@
+__ZN12HGApply3DLUT4InitEm8HGFormatffbNS_29hgApply3DLUTInterpolationTypeEbbbb:
+0000000000072ad0	pushq	%rbp
+0000000000072ad1	movq	%rsp, %rbp
+0000000000072ad4	movss	0x35a20c(%rip), %xmm3
+0000000000072adc	movss	0x35a208(%rip), %xmm4
+0000000000072ae4	movss	0x35a204(%rip), %xmm5
+0000000000072aec	xorps	%xmm2, %xmm2
+0000000000072aef	popq	%rbp
+0000000000072af0	jmp	__ZN12HGApply3DLUT4InitEm8HGFormatffffffbNS_29hgApply3DLUTInterpolationTypeEbbbb ## HGApply3DLUT::Init(unsigned long, HGFormat, float, float, float, float, float, float, bool, HGApply3DLUT::hgApply3DLUTInterpolationType, bool, bool, bool, bool)
+0000000000072af5	nopw	%cs:(%rax,%rax)
